66047c14b3994ddd612733788ce889a675dfda96
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-21 13:00:06 +0200

    Obteniendo las recetas por el state del context

--- src/context/RecetasContext.js
+++ src/context/RecetasContext.js
@@ -20,7 +20,11 @@ useEffect(() => {
             const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${nombre}&
             c=${categoria}`;
 
-            console.log(url);
+            const resultado = await axios.get(url);
+
+            // console.log(resultado.data.drinks);
+
+            guardarRecetas(resultado.data.drinks);
             
         }
         obtenerRecetas();
