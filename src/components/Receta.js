78ff02532998ef1e9f1607fb9494db76288e34c7
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-21 18:39:13 +0200

    Importando un componente desde material-ui

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,6 +1,30 @@
 import React, { useContext } from 'react';
 import { ModalContext } from '../context/ModalContext';
 
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+
+function getModalStyle() {
+    const top = 50 ;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+}));
+
 
 const Receta = ({receta}) => {
 
