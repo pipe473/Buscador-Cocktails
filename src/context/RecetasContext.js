98a9a8dacb33c02f0b07a1bf606ead7ad798f099
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-21 12:16:16 +0200

    Aplicando destructuring a la llamada de la api

--- src/context/RecetasContext.js
+++ src/context/RecetasContext.js
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
 export const RecetasContext = createContext();
 
 const RecetasProvider = (props) => {
@@ -8,7 +8,16 @@ const [ recetas, guardarRecetas ] = useState([]);
 const [ busqueda, buscarRecetas ] = useState({
     nombre: '',
     categoria: ''
-})
+});
+
+const { nombre, categoria } = busqueda;
+
+useEffect(() => {
+    const obtenerRecetas = async () => {
+        const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${nombre}&
+        c=${categoria}`;
+    }
+}, [busqueda]);
 
     return ( 
         <RecetasContext.Provider
