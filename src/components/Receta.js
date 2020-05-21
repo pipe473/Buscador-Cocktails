c7a603db97bb9d6d6794052b59a3bca0dc08dc63
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-21 13:32:55 +0200

    Añadiendo la imagen de la bebida

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -5,6 +5,9 @@ const Receta = ({receta}) => {
        <div className="col-md-4 mb-3">
            <div className="card">
                 <h2 className="card-header">{receta.strDrink}</h2>
+
+                <img className="card-img-top" src={receta.strDrinkThumb} alt={`Imagen de 
+                ${receta.strDrink}`} />
            </div>
        </div> 
      );
